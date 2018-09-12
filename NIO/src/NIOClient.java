a968a860f0f648cd7ac905d3bddb87a09f48a33c
kanon <vipkia@sina.cn>
2018-09-12 15:36:53 +0800

    I/O 多路复用

--- NIO/src/NIOClient.java
+++ NIO/src/NIOClient.java
@@ -1,31 +1,114 @@
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
+import java.util.Iterator;
 
 /**
  * @author yinan
  * @date 18-9-11
  */
 public class NIOClient {
+    private static Selector selector;
+    static {
+        try {
+            selector = Selector.open();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         try {
             SocketChannel socketChannel = SocketChannel.open();
-            boolean flag = socketChannel.connect(new InetSocketAddress("127.0.0.1", 8888));
-            String request = "hello world";
-            System.out.println("发送： " + request);
-            byte[] bytes = request.getBytes(StandardCharsets.UTF_8);
-            ByteBuffer writeBuffer = ByteBuffer.wrap(bytes);
-            socketChannel.write(writeBuffer);
-            ByteBuffer buffer = ByteBuffer.allocate(100);
-            socketChannel.read(buffer);
-            byte[] data = buffer.array();
-            String message = new String(data);
-            System.out.println(message);
+            socketChannel.configureBlocking(false);
+            socketChannel.connect(new InetSocketAddress("www.baidu.com", 80));
+            socketChannel.register(selector, SelectionKey.OP_CONNECT | SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+            NIOClient client = new NIOClient();
+            //selector.select() 是同步阻塞的
+            while (selector.select() > 0) {
+                Iterator<SelectionKey> keys = selector.selectedKeys().iterator();
+                while (keys.hasNext()) {
+                    SelectionKey key = keys.next();
+                    keys.remove();
+                    if (key.isConnectable()) {
+                        client.connect(key);
+                    } else if (key.isReadable()) {
+                        client.receive(key);
+                    } else if (key.isWritable()) {
+                        client.write(key);
+                    }
+                }
+            }
             socketChannel.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    /**
+     * 处理连接事件
+     * @param key
+     * @throws IOException
+     */
+    private void connect(SelectionKey key) throws IOException {
+        //获取事件句柄对应的channel
+        SocketChannel channel = (SocketChannel) key.channel();
+        //完成真正的socket连接
+        channel.finishConnect();
+        //打印连接信息
+        InetSocketAddress remote = (InetSocketAddress) channel.socket().getRemoteSocketAddress();
+        String host = remote.getHostName();
+        int port = remote.getPort();
+        System.out.println(String.format("访问地址： %s:%s 连接成功!", host, port));
+
+    }
+
+    /**
+     * 处理写入就绪事件
+     * @param key
+     * @throws IOException
+     */
+    private void write(SelectionKey key) throws IOException {
+        SocketChannel channel = (SocketChannel) key.channel();
+        InetSocketAddress remote = (InetSocketAddress) channel.socket().getRemoteSocketAddress();
+        String host = remote.getHostName();
+        Charset charset = Charset.forName("utf-8");
+        //获取http请求数据
+        String request = compositeRequest(host);
+        //向SocketChannel中写入事件
+        channel.write(charset.encode(request));
+        //修改SocketChannel所关心的事件
+        key.interestOps(SelectionKey.OP_READ);
+
+    }
+
+    private void receive(SelectionKey key) throws IOException {
+        SocketChannel channel = (SocketChannel) key.channel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        channel.read(buffer);
+        buffer.flip();
+        Charset charset = Charset.forName("utf-8");
+        String receiveData = charset.decode(buffer).toString();
+        //当没有数据可读时，取消在选择器中关联，并关闭socket连接
+        if ("".equals(receiveData)) {
+            key.channel();
+            channel.close();
+            return;
+        }
+        System.out.println(String.format("服务端返回信息： %s",  receiveData));
+    }
+
+    private static String compositeRequest(String host){
+
+        return "GET / HTTP/1.1\r\n" +
+                "Host: " + host + "\r\n" +
+                "User-Agent: curl/7.43.0\r\n" +
+                "Accept: */*\r\n\r\n";
+//        return "Hello world!";
+    }
+
+
 }
--- NIO/src/NIOServer.java
+++ NIO/src/NIOServer.java
@@ -5,6 +5,7 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 
@@ -19,28 +20,32 @@ public class NIOServer {
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
         //绑定端口
         serverSocketChannel.socket().bind(
-                new InetSocketAddress("127.0.0.1", 8888)
+                new InetSocketAddress("127.0.0.1", 8881)
         );
         //设置通道为非阻塞
         serverSocketChannel.configureBlocking(false);
         Selector selector = Selector.open();
         //注册channel到selector
         serverSocketChannel.register(selector,  SelectionKey.OP_ACCEPT);
-        while (true) {
-            selector.select();
+        while (selector.select() > 0) {
             //罗列所有的channel状态
             Iterator<SelectionKey> iterable = selector.selectedKeys().iterator();
             while (iterable.hasNext()) {
                 SelectionKey key = iterable.next();
                 iterable.remove();
+                NIOServer server = new NIOServer();
                 if (key.isAcceptable()) {
                     //获取满足条件的channel 即 OP_ACCEPT
                     ServerSocketChannel socketChannel = (ServerSocketChannel) key.channel();
+                    //服务端接收一个客户端连接
                     SocketChannel channel = socketChannel.accept();
                     channel.configureBlocking(false);
+                    //注册这个客户端连接状态
                     channel.register(selector, SelectionKey.OP_READ);
                 } else if (key.isReadable()){
-                    recive(key);
+                    server.receive(key);
+                } else if (key.isWritable()) {
+                    server.write(key);
                 }
             }
 
@@ -49,7 +54,7 @@ public class NIOServer {
 
     }
 
-    private static void recive(SelectionKey key) {
+    private static void receive1(SelectionKey key) {
         SocketChannel channel = (SocketChannel) key.channel();
         try {
             ByteBuffer buffer =  ByteBuffer.allocate(5 * 1024);
@@ -66,6 +71,7 @@ public class NIOServer {
                     break;
                 }
             }
+            //切换状态，如果一次没有读完，接着读
             buffer.flip();
             String request = new String(buffer.array());
             System.out.println(request);
@@ -76,6 +82,47 @@ public class NIOServer {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 处理写入就绪事件
+     * @param key
+     * @throws IOException
+     */
+    private void write(SelectionKey key) throws IOException {
+        SocketChannel channel = (SocketChannel) key.channel();
+        InetSocketAddress remote = (InetSocketAddress) channel.socket().getRemoteSocketAddress();
+        String host = remote.getHostName();
+        Charset charset = Charset.forName("utf-8");
+        //获取http请求数据
+        String request = compositeRequest(host);
+        //向SocketChannel中写入事件
+        channel.write(charset.encode(request));
+        //修改SocketChannel所关心的事件
+        key.interestOps(SelectionKey.OP_READ);
+
+    }
+
+    private void receive(SelectionKey key) throws IOException {
+        SocketChannel channel = (SocketChannel) key.channel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        channel.read(buffer);
+        buffer.flip();
+        Charset charset = Charset.forName("utf-8");
+        String receiveData = charset.decode(buffer).toString();
+        //当没有数据可读时，取消在选择器中关联，并关闭socket连接
+        if ("".equals(receiveData)) {
+            key.channel();
+            channel.close();
+            return;
+        }
+        System.out.println(String.format("客户端发送信息: %s", receiveData));
+        key.interestOps(SelectionKey.OP_WRITE);
+    }
+
+    private static String compositeRequest(String host){
+
+        return "明天，你好";
     }
 
 }
